b13ef60cf16cd0ba590c238d09c47409cade3230
Antonio Giustino <31846783+Superjolly002@users.noreply.github.com>
2020-01-22 14:41:52 +0100

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,11 @@
+package com.mycompany.hello_world;
+
+public class hello_world {
+
+    public static void main(String[] args) {
+    
+        System.out.print("Hello World");
+        
+    }
+    
+}
